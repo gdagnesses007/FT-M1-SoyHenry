c14e927a92139d72cf8e263831797f9b48d93f75
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-07-06 20:32:20 +0200

    tareas resueltas

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let dec = 0;
+  for (let i = 0; i < num.length; i++) {
+    dec += num[i] * Math.pow(2, num.length - 1 - i);
+  }
+  return dec;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let bin = '';
+  while (num > 0) {
+    bin = num % 2 + bin;
+    num = Math.floor(num / 2);  
+  }
+  return bin;
 }
 
 
