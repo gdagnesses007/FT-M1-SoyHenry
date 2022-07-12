059d9d554edda648743f912629646fa7b53f1470
Guillermo Dagnesses Segura <gdagnesses007@gmail.com>
2022-07-11 17:32:15 -0700

    casi listo... falta el ultimo metodo .breadthFirstForEach()

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,119 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function () {
+  let size = 0;
+  if (this.isLeaf()) {
+    return 1;
+  }
+  if (this.left) {
+    size += this.left.size();
+  }
+  if (this.right) {
+    size += this.right.size();
+  }
+  return size + 1;
+}
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value <= this.value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (this.left && value < this.value) {
+    return this.left.contains(value);
+  }
+  if (this.right && value > this.value) {
+    return this.right.contains(value);
+  }
+  return false;
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (callback, dfs = 'in-order') {
+  //let result = [];
+  switch (dfs) {
+    case 'post-order':
+      if (this.left) {
+        //result = [...result, ...this.left.depthFirstForEach(dfs)];
+        this.left.depthFirstForEach(callback, dfs);
+      }
+      if (this.right) {
+        //result = [...result, ...this.right.depthFirstForEach(dfs)];
+        this.right.depthFirstForEach(callback, dfs);
+      }
+      //result.push(this.value);
+      callback(this.value);
+      break;
+    case 'pre-order':
+      //result.push(this.value);
+      callback(this.value);
+      if (this.left) {
+        //result = [...result, ...this.left.depthFirstForEach(dfs)];
+        this.left.depthFirstForEach(callback, dfs);
+      }
+      if (this.right) {
+        //result = [...result, ...this.right.depthFirstForEach(dfs)]
+        this.right.depthFirstForEach(callback, dfs);
+      }
+      break;
+    case 'in-order':
+      if (this.left) {
+        //result = [...result, ...this.left.depthFirstForEach(dfs)];
+        this.left.depthFirstForEach(callback, dfs);
+      }
+      //result.push(this.value);
+      callback(this.value);
+      if (this.right) {
+        //result = [...result, ...this.right.depthFirstForEach(dfs)]
+        this.right.depthFirstForEach(callback, dfs);
+      }
+      break;
+  }
+  //return result;
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function (callback) {
+  //let result = [];
+  let queue = [];
+  queue.push(this);
+  while (queue.length > 0) {
+    let tree = queue.shift();
+    //result.push(tree.value);
+    callback(tree.value);
+    if (tree.left) {
+      queue.push(tree.left);
+    }
+    if (tree.right) {
+      queue.push(this.right);
+    }
+  }
+  //return result;
+}
+
+BinarySearchTree.prototype.isLeaf = function () {
+  return !this.left && !this.right;
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
